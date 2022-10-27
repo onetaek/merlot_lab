a7038472a49077d9aa1741bfae0642ed0b1d3606
oh one taek <dnjsxorchlrh@naver.com>
2022-10-27 17:39:45 +0900

    slick 시도실패

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".variable-width").slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+    });
+  });
